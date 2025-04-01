a79247b93c00adab4e05bc28668a3b6d32cb4b2e
Artur Luniaka <artur01049718@gmail.com>
2025-04-01 08:47:18 +0100

    added routes companies and dashboard/add fonts/clean start code

---
+++ src/app/(companies)/companies/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+const page = () => {
+  return (
+    <div>
+      <h1>Companies</h1>
+    </div>
+  );
+};
+
+export default page;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mdx: "1280px",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          mdx: "2rem",
+        },
+      },
+      fontFamily: {
+        inter: ["Inter"],
+        jakarta: ["Plus Jakarta Sans"],
+        poppins: ["Poppins"],
+      },
+    },
   },
   plugins: [],
 };
